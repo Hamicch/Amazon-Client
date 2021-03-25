6b3c1ec79dd9cf92a89629382b52549026687967
hamicch <hamichtech@gmail.com>
2021-03-25 09:24:45 +0100

    added footer component and creatured featured product

---
+++ components/FeatureProduct.vue
@@ -0,0 +1,36 @@
+<template>
+        <div class="mianContent">
+                <div class="unified_widget pageBanner">
+                <h1>Books</h1>
+
+                <p>
+                        <b>
+                                Browse for more Books
+                                <a href="#">leaders of notable people</a>, or
+                                <a href="#">fantacy books</a> liske harry potter or Game of thrones
+                                <a href="#">Best Fantasy books of the yera so far</a>
+                        </b>
+                </p>
+        </div>
+        <div class="bx-root">
+                <div class="row">
+                        <div class="col-ms-5 text-container">
+                                <div class="bx-root-image">
+                                        <img src="/img/FeaturedProduct.jpg" alt="featured-product" class="img-fluid">
+                                </div>
+                        </div>
+                        <div class="col-sm-7 m-auto">
+                                <div class="bx-root-dtls">
+                                        <div class="bx-root heading">The Program</div>
+                                </div>
+                                <div class="bx-root-subtext">
+                                        Inside the mind of the lengendary Author, Author JK Rowling
+                                </div>
+                                <div class="bx-root-link">
+                                        <span>Order Now</span>
+                                </div>
+                        </div>
+                </div>
+        </div>
+        </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,21 @@
   <div>
     <Navbar />
     <Nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
 
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
